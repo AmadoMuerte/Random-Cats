febc6cce5e2182047d49ea3331405d7b19713580
Amado <kisik854@gmail.com>
2022-04-03 17:40:03 +1000

    create app random cats

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const url = 'https://api.thecatapi.com/v1/images/search';
+
+function sendRequest(method, url, body = null) {
+    return fetch(url).then(response => {
+        return response.json()
+    })
+}
+
+
+
+
+// .catch(err => console.log(err));
+
+document.querySelector('.btn').addEventListener('click', () => {
+    sendRequest('GET', url)
+    .then(data => {
+        document.querySelector('.image').src = data[0].url;
+    });
+});
